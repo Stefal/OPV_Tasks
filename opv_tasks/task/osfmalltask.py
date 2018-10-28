51574d2371a92e378fbe29fac4bf1558b7f0a8c4
Benjamin BERNARD <benjamin.bernard@openpathview.fr>
2018-10-28 18:07:22 +0100

    Saving OpenSFM reconstruction to a specific folder

--- opv_tasks/task/osfmalltask.py
+++ opv_tasks/task/osfmalltask.py
@@ -1,6 +1,10 @@
 from opv_tasks.task import Task
 from opv_tasks.utils import runTask
-import tempfile
+from opv_tasks.const import Const
+from opv_api_client import ressources
+from path import Path
+
+import datetime
 
 
 class OsfmallTask(Task):
@@ -12,9 +16,18 @@ class OsfmallTask(Task):
     TASK_NAME = "osfmall"
     requiredArgsKeys = ["id_malette", "ids_pano"]
 
+    def getReconstructionFolder(self, id_panorama, id_malette):
+        panoramaSensor = self._client_requestor.make(ressources.PanoramaSensors, id_panorama, id_malette)
+        id_campaign = panoramaSensor.id_campaign
+
+        date = datetime.datetime.now().strftime("%Y-%m-%d_%Hh%M")
+        dir = Path(Const.OPENSFM_RECONSTRUCTION_FOLDER) / "{id_campaign}-{id_malette}_{date}".format(id_campaign=id_campaign, id_malette=id_malette, date=date)
+        dir.makedirs()
+        return dir
+
     def runWithExceptions(self, options={}):
         self.checkArgs(options)
-        options["osfm_dir"] = tempfile.mkdtemp()
+        options["osfm_dir"] = self.getReconstructionFolder(id_panorama=options["ids_pano"][0], id_malette=options["id_malette"])
         self.logger.info("Osfm dir : {}".format(options["osfm_dir"]))
         self.logger.info("Launching task osfm extract")
         runTask(self._opv_directory_manager, self._client_requestor, "osfmextract", options)
